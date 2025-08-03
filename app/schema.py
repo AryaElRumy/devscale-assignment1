612fc055ee822e7bb8e99ddd3e2ae08e2e0e618c
Mochamad Arya El Rumy <personalrumy@gmail.com>
2025-08-03 21:23:26 +0700

    allow address in RegisterStudent to be null

--- app/schema.py
+++ app/schema.py
@@ -1,10 +1,12 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class RegisterStudent(BaseModel):
     name: str
     grade: int
-    address: str
+    address: Optional[str] | None
 
 
 class ViewStudents(BaseModel):
